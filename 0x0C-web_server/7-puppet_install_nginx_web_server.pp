4008ae52935ea4e7ffa4ee5067998f55c798d5f5
deenyshomar <deenyshomar@gmail.com>
2024-03-17 15:06:40 +0100

    fixing

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -14,7 +14,7 @@ file {'/var/www/html/index.html':
 }
 
 exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://google.com permanent;" /etc/nginx/sites-available/default',
 	provider => 'shell'
 }
 
